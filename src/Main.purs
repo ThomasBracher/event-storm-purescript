6269b438ad88ef368a0588e6ea22e47babbe7f49
Thomas Bracher <thomas@hopwork.com>
2018-09-18 21:03:22 +0200

    version after first session

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,46 @@
 module Main where
 
 import Prelude
-import Effect (Effect)
-import Effect.Console (log)
 
-main :: Effect Unit
-main = do
-  log "Hello sailor!"
+data ColumnTransition
+  = TodoToOngoing
+  | OngoingToFinished
+
+type Body = String
+type Date = String
+type CardId = String
+
+data Event
+  = CardMoved CardId Date ColumnTransition
+  | CardBodyChanged CardId Date Body
+  | CardAdded CardId Date Body
+  | CardRemoved CardId Date
+  | CardArchived CardId Date
+
+-- Commande -> Evenement -> Modèle de lecture
+
+type Card =
+  { id :: CardId
+  , body :: Body
+  }
+type OnGoingCard =
+  { id :: CardId
+  , body :: Body
+  , tooLong :: Boolean
+  }
+type KanbanBoard =
+  { todo :: Array Card
+  , onGoing :: Array OnGoingCard
+  , done :: Array Card
+  }
+
+fun :: Event -> Event -> Unit
+fun (CardAdded cardIdLeft _ _) (CardAdded cardIdRight _ _) = unit
+fun _ _ = unit
+
+eventListener :: KanbanBoard -> Event -> KanbanBoard
+eventListener { todo, onGoing, done } (CardAdded cardId date body) =
+  { todo: [{ id: cardId, body: body }] <> todo
+  , onGoing
+  , done }
+eventListener board _  = board
--- test/Main.purs
+++ test/Main.purs
@@ -3,7 +3,10 @@ module Test.Main where
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
+import Test.Assert (assert)
+import Main
 
 main :: Effect Unit
 main = do
-  log "You should add some tests."
+  assert $ eventListener { todo: [], onGoing: [], done: []} (CardAdded "cardId" "date" "body") == {todo: [{id: "cardId", body: "body"}], onGoing: [], done:[]}
+
